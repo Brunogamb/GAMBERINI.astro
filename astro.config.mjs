1d66402f1cdab68bda36d9f0cbc16d23918d0a70
Brunogamb <brunogamberini24@gmail.com>
2025-08-29 20:27:19 -0300

    Refactor sitemap integration in configuration file for improved clarity

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-    site: "https://www.gamberini.com.ar/",
-    integrations: [sitemap()],
+  site: 'https://www.gamberini.com.ar',
+  integrations: [
+    sitemap({
+      filter: (page) => !page.includes('404'),
+      filename: 'sitemap.xml',                 
+    }),
+  ],
 });
