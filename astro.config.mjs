f566f666421aa2164cb57511362535e129624a14
Brunogamb <brunogamberini24@gmail.com>
2025-08-29 20:35:43 -0300

    Remove duplicate filename entry in sitemap integration for cleaner configuration and build of project

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ export default defineConfig({
   site: 'https://www.gamberini.com.ar',
   integrations: [
     sitemap({
+      filename: 'sitemap.xml',
       filter: (page) => !page.includes('404'),
-      filename: 'sitemap.xml',                 
     }),
   ],
 });
