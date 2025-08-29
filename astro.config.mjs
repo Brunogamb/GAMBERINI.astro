11133e19305eb0a20fcf0be3c0972666ba35d509
Brunogamb <brunogamberini24@gmail.com>
2025-08-29 20:23:18 -0300

    Add sitemap integration and update site URL for improved SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://gamberini.com.ar/",
+    site: "https://www.gamberini.com.ar/",
+    integrations: [sitemap()],
 });
